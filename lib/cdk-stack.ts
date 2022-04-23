b0e6dfb228cf3526bea4f4cadee92fb77bc841d1
Hassan Mohamed <hassan.mhmd656@gmail.com>
2022-04-24 01:11:48 +0200

    adding pillow library & image resizing to store it in thumbS3Bucket

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -11,24 +11,46 @@ import event_sources = require('@aws-cdk/aws-lambda-event-sources');
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 const imageBucketName = "cdk-hsn-imagebucket"
-
+const resizedBucketName = imageBucketName + "-resized"
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
     // =====================================================================================
     // Image Bucket
     // =====================================================================================
-    const imageBucket = new s3.Bucket(this, imageBucketName);
+    const imageBucket = new s3.Bucket(this, imageBucketName,{
+      removalPolicy:cdk.RemovalPolicy.DESTROY
+    });
     new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
+
+    // =====================================================================================
+    // Thumbnail Bucket
+    // =====================================================================================
+    const resizedBucket = new s3.Bucket(this, resizedBucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    new cdk.CfnOutput(this, 'resizedBucket', {value: resizedBucket.bucketName});
 
     // =====================================================================================
     // Amazon DynamoDB table for storing image labels
     // =====================================================================================
     const table = new dynamodb.Table(this, 'ImageLabels', {
       partitionKey: { name: 'image', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
     });
     new cdk.CfnOutput(this, 'ddbTable', { value: table.tableName });
+
+    // =====================================================================================
+    // Building our AWS Lambda Function; compute for our serverless microservice
+    // =====================================================================================
+    const layer = new lambda.LayerVersion(this, 'pil', {
+      code: lambda.Code.fromAsset('reklayer'),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_7],
+      license: 'Apache-2.0',
+      description: 'A layer to enable the PIL library in our Rekognition Lambda',
+    });
 
     // =====================================================================================
     // Building our AWS Lambda Function; compute for our serverless microservice
@@ -39,14 +61,17 @@ export class CdkStack extends cdk.Stack {
       handler: 'index.handler',
       timeout: Duration.seconds(30),
       memorySize: 1024,
+      layers:[layer],
       environment: {
           "TABLE": table.tableName,
           "BUCKET": imageBucket.bucketName,
+          "RESIZEDBUCKET": resizedBucket.bucketName
       },
     });
 
     rekFn.addEventSource(new event_sources.S3EventSource(imageBucket,{events:[s3.EventType.OBJECT_CREATED]}))
     imageBucket.grantRead(rekFn);
+    resizedBucket.grantPut(rekFn);
     table.grantWriteData(rekFn);
 
     rekFn.addToRolePolicy(new iam.PolicyStatement({
